4db91d3c48f613291116c53397b553324c14f01d
奥村彰良 <aonk13965@gmail.com>
2020-02-10 13:28:15 +0900

    javascrip修正完了

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -1,112 +1,68 @@
 document.addEventListener('turbolinks:load', () => {
-    const today = new Date(new Date().setHours(0, 0, 0, 0))
-    const a_month_ago = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate())
+    // '2020-01-12'のような文字列から，Javascriptの日付オブジェクトを取得する関数
+    // setHoursを使用しないと，時差の影響で0時にならないため注意！
+    const convertDate = (date) => new Date(new Date(date).setHours(0, 0, 0, 0))
 
-    // ● カレンダー
+    const TODAY = convertDate(new Date())
+    const A_WEEK_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth(), TODAY.getDate() - 6)
+    const TWO_WEEKS_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth(), TODAY.getDate() - 13)
+    const A_MONTH_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth() - 1, TODAY.getDate() + 1)
+    const THREE_MONTHS_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth() - 3, TODAY.getDate() + 1)
 
-    // 選択できない日付データ（自由に変更していただいてOKです）
-    const disable_dates = ['2019-12-10', '2019-12-20', '2019-12-30', '2020-01-10', '2020-1-20', '2020-01-30']
+    // グラフを描く場所を取得
+    const chartWeightContext = document.getElementById("chart-weight").getContext('2d')
 
-    // カレンダーの日本語化
-    flatpickr.localize(flatpickr.l10ns.ja);
+    // 期間を指定してグラフを描く
+    const drawGraph = (from, to) => {
+        // from から to までの期間のデータに絞る
+        let records = gon.weight_records.filter((record) => {
+            let date = convertDate(record.date)
+            return from <= date && date <= to
+        })
 
-    // カレンダーの表示
-    flatpickr('#date-form', {
-        // スマートフォンでもカレンダーに「flatpickr」を使用
-        disableMobile: true,
-        // 1ヶ月前から本日まで選択可
-        minDate: a_month_ago,
-        maxDate: today,
-        // 選択できない日付
-        disable: disable_dates
-    });
+        // 日付のみのデータを作成
+        let dates = records.map((record) => {
+            // 横軸のラベル表示は簡潔にしたいので，
+            // 日付 2020-01-08 を 1/8 のような形式に変換する
+            return record.date.replace(/^\d+-0*(\d+)-0*(\d+)$/, '$1/$2')
+        })
 
-    // ***********************************
-    // ● グラフ
-    // 棒グラフのデータ（自由に変更していただいてOKです）
-    let barLabel = ['1月', '2月', '3月', '4月', '5月', '6月']
-    let barData = [5, 4, 2, 6, 5, 8]
+        // 体重のみのデータを作成
+        let weights = records.map((record) => record.weight)
 
-    // 折れ線グラフのデータ（自由に変更していただいてOKです）
-    let lineLabel = gon.chart_label
-    let lineData = gon.chart_data
-
-    // 棒グラフのオプション
-
-    const barChartData = {
-        labels: barLabel,
-        datasets: [{
-            label: '得点',
-            data: barData,
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            borderColor: 'rgba(54, 162, 235, 1)',
-            borderWidth: 1
-        }]
-    }
-
-    const barChartOption = {
-        title: {
-            display: true,
-            text: '棒グラフ'
-        },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    // y軸のメモリを 0 からスタートに強制
-                    beginAtZero: true
-                }
+        let weightData = {
+            labels: dates,
+            datasets: [{
+                label: '体重(kg)',
+                data: weights,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1,
+                spanGaps: true
             }]
         }
-    }
 
-    // 折れ線グラフのオプション
-
-    const lineChartData = {
-        labels: lineLabel,
-        datasets: [{
-            label: '体重(kg)',
-            data: lineData,
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)',
-            borderWidth: 1,
-            spanGaps: true
-        }]
-    }
-
-    const lineChartOption = {
-        title: {
-            display: true,
-            text: '折れ線グラフ'
-        },
-        tooltips: {
-            callbacks: {
-                // ホバー（スマホならタップ）時のラベル表示を変更
-                title: function(tooltipItems) {
-                    return tooltipItems[0].xLabel.replace(/^(\d+).(\d+)$/, ' $1 月 $2 日')
-                },
-                label: function(tooltipItem) {
-                    return '体重: ' + tooltipItem.yLabel + 'kg'
+        let weightOption = {
+            tooltips: {
+                callbacks: {
+                    // ホバー（スマホならタップ）時のラベル表示を変更
+                    title: function(tooltipItems) {
+                        return tooltipItems[0].xLabel.replace(/^(\d+).(\d+)$/, ' $1 月 $2 日')
+                    },
+                    label: function(tooltipItem) {
+                        return '体重: ' + tooltipItem.yLabel + 'kg'
+                    }
                 }
             }
         }
+
+        new Chart(chartWeightContext, {
+            type: 'line',
+            data: weightData,
+            options: weightOption
+        })
     }
 
-    // 棒グラフを表示
-    const barChartContext = document.getElementById("bar-chart").getContext('2d')
-    new Chart(barChartContext, {
-        type: 'bar',
-        data: barChartData,
-        options: barChartOption
-    })
-
-    // 折れ線グラフを表示
-    const lineChartContext = document.getElementById("line-chart").getContext('2d')
-    new Chart(lineChartContext, {
-        type: 'line',
-        data: lineChartData,
-        options: lineChartOption
-    })
-
-
-
+    // グラフの初期表示
+    drawGraph(A_WEEK_AGO, TODAY)
 })
